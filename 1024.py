7a8e3ca22c14e6cf5e78452637b8aeb384edab65
zgary5182-hash <zgary5182@gmail.com>
2026-08-03 03:41:01 +0800

    Create 1024.py

---
+++ 1024.py
@@ -0,0 +1,8 @@
+from itertools import product
+
+vals = [1, 2, 3, 4, -1, -2, -3, -4]
+puzzles = list(product([1, 2, 3, 4], [1, 2, 3, 4], vals, vals))
+
+with open("puzzles.txt", "w", encoding="utf-8") as f:
+    for p in puzzles:
+        f.write(" ".join(map(str, p)) + "\n")
