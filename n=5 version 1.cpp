af9e52bb4c677b83035593c4ba4548d4fd6c7fda
shawnh1h <shawnhuanghh@outlook.com>
2026-08-03 15:18:57 +0800

    New verison of dfs

--- n=5 version 1.cpp
+++ n=5 version 1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <ctime>
 #include <cmath>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -25,12 +29,21 @@ struct Cell
     Piece p;
 };
 
+struct Candidate
+{
+    int id;
+    int rot;
+};
+
 Piece pieces[N * N];
 Piece rot[N * N][4];
 
 Cell board[N][N];
 
 bool used[N * N];
+
+unordered_map<int, vector<Candidate> > top_index;
+unordered_map<int, vector<Candidate> > left_index;
 
 long long nodes = 0;
 
@@ -62,6 +75,27 @@ void build_rotations()
             rot[i][r] = rotate90(rot[i][r - 1]);
     }
 }
+
+void build_index()
+{
+    for (int i = 0; i < n * n; i++)
+    {
+        for (int k = 0; k < 4; k++)
+        {
+            Candidate candidate;
+
+            candidate.id = i;
+            candidate.rot = k;
+
+            int top_color = abs(rot[i][k].e[0].value);
+            int left_color = abs(rot[i][k].e[3].value);
+
+            top_index[top_color].push_back(candidate);
+            left_index[left_color].push_back(candidate);
+        }
+    }
+}
+
 bool valid(int r, int c)
 {
     Piece &cur = board[r][c].p;
@@ -81,6 +115,33 @@ bool valid(int r, int c)
     return true;
 }
 
+bool dfs(int pos);
+
+bool try_candidate(int pos, int r, int c, Candidate candidate)
+{
+    int i = candidate.id;
+    int k = candidate.rot;
+
+    if (used[i])
+        return false;
+
+    board[r][c].id = i;
+    board[r][c].rot = k;
+    board[r][c].p = rot[i][k];
+
+    if (!valid(r, c))
+        return false;
+
+    used[i] = true;
+
+    if (dfs(pos + 1))
+        return true;
+
+    used[i] = false;
+
+    return false;
+}
+
 bool dfs(int pos)
 {
     nodes++;
@@ -91,27 +152,57 @@ bool dfs(int pos)
     int r = pos / n;
     int c = pos % n;
 
-    for (int i = 0; i < n * n; i++)
+    if (r == 0 && c == 0)
     {
-        if (used[i])
-            continue;
-
-        for (int k = 0; k < 4; k++)
+        for (int i = 0; i < n * n; i++)
         {
-            board[r][c].id = i;
-            board[r][c].rot = k;
-            board[r][c].p = rot[i][k];
-
-            if (valid(r, c))
+            for (int k = 0; k < 4; k++)
             {
-                used[i] = true;
+                Candidate candidate;
 
-                if (dfs(pos + 1))
+                candidate.id = i;
+                candidate.rot = k;
+
+                if (try_candidate(pos, r, c, candidate))
                     return true;
-
-                used[i] = false;
             }
         }
+
+        return false;
+    }
+
+    vector<Candidate> *candidates;
+
+    if (r > 0 && c > 0)
+    {
+        int top_color = abs(board[r - 1][c].p.e[2].value);
+        int left_color = abs(board[r][c - 1].p.e[1].value);
+
+        vector<Candidate> &top_candidates = top_index[top_color];
+        vector<Candidate> &left_candidates = left_index[left_color];
+
+        if (top_candidates.size() < left_candidates.size())
+            candidates = &top_candidates;
+        else
+            candidates = &left_candidates;
+    }
+    else if (r > 0)
+    {
+        int top_color = abs(board[r - 1][c].p.e[2].value);
+
+        candidates = &top_index[top_color];
+    }
+    else
+    {
+        int left_color = abs(board[r][c - 1].p.e[1].value);
+
+        candidates = &left_index[left_color];
+    }
+
+    for (size_t i = 0; i < candidates->size(); i++)
+    {
+        if (try_candidate(pos, r, c, (*candidates)[i]))
+            return true;
     }
 
     return false;
@@ -119,17 +210,34 @@ bool dfs(int pos)
 
 int main()
 {
-    clock_t start = clock();
-
     cout << "Puzzle size = " << n << "x" << '\n';
-    cout << "Enter " << n * n << " pieces:\n";
-    cout << "Each piece: 4 integers (positive/negative)\n\n";
+    cout << "Enter input file path:\n";
+
+    string file_path;
+
+    getline(cin, file_path);
+
+    ifstream input_file(file_path);
+
+    if (!input_file)
+    {
+        cout << "\nCannot open input file\n";
+        return 1;
+    }
 
     for (int i = 0; i < n * n; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            cin >> pieces[i].e[j].value;
+            if (!(input_file >> pieces[i].e[j].value))
+            {
+                cout << "\nInvalid input file\n";
+                cout << "The file must contain "
+                     << n * n
+                     << " pieces, each with 4 integers\n";
+
+                return 1;
+            }
         }
     }
 
@@ -137,6 +245,9 @@ int main()
         used[i] = false;
 
     build_rotations();
+    build_index();
+
+    clock_t start = clock();
 
     if (dfs(0))
     {
